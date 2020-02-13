6db45e7793ff983ba2dfb4acb5ad350ef4507e49
BuildTools <unconfigured@null.spigotmc.org>
2020-02-13 15:50:47 -0500

    changed mysql account in connection strings

--- code/easysched/Data/easyschedContext.cs
+++ code/easysched/Data/easyschedContext.cs
@@ -29,7 +29,7 @@ namespace easysched.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=password;database=easysched", x => x.ServerVersion("8.0.19-mysql"));
+                optionsBuilder.UseMySql("server=localhost;port=3306;user=easysched;password=easysched2020;database=easysched", x => x.ServerVersion("8.0.19-mysql"));
             }
         }
 
